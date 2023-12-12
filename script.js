b1fbf8cb60a1c8f95bb4ba33d6e85770b59300bb
Stanley Azi <azistanley17@gmail.com>
2023-12-12 23:26:46 +0100

    new commit

--- script.js
+++ script.js
@@ -46,5 +46,106 @@
 
 // formValidation('#contact_form');
 
+//switching between forms
+
+// //login
+// const loginHealthExplorer = document.querySelector('.health_explorer_lg'); //gets login Health Explorer heading
+// const loginHealthPartner = document.querySelector('.health_partner_lg'); //gets login Health Partner heading
+
+// const healthExplorerLoginForm = document.getElementById('health_explorer_form_lg'); //gets Health Explorer login form
+// const healthPartnerLoginForm = document.getElementById('health_partner_form_lg'); //gets Health Partner login form 
+
+// const healthExplorerLoginStyle = window.getComputedStyle(healthExplorerLoginForm); 
+// const healthPartnerLoginStyle = window.getComputedStyle(healthPartnerLoginForm);
 
 
+// //sign_up
+const signUphealthExplorer = document.querySelector('.health_explorer_sp'); //gets signup Health Explorer heading
+const signUpHealthPartner = document.querySelector('.health_partner_sp'); //gets signup Health Partner heading
+
+const healthExplorerSignUpForm = document.getElementById('health_explorer_form_sp'); //gets Health Explorer sign up form
+const healthPartnerSignUpForm = document.getElementById('health_partner_form_sp'); //gets Health Partner sign up form 
+
+const healthExplorerSignUpStyle = window.getComputedStyle(healthExplorerSignUpForm); 
+const healthPartnerSignUpStyle = window.getComputedStyle(healthPartnerSignUpForm);
+
+
+
+//switch between form function
+// const switchForm = function (explorer, partner, explorerStyle, partnerStyle, explorerForm, partnerForm) {
+//     partner.addEventListener('click', function () {
+//         if (partnerStyle.getPropertyValue('display') === 'none') {
+//             partnerForm.style.display = 'flex';
+//             partner.style.background = '#1E1E1E';
+//             partner.style.color = "#FFFFFF";
+            
+//             explorerForm.style.display = 'none';
+//             explorer.style.color = '#1E1E1E';
+//             explorer.style.background = '#FFFFFF';
+//         } 
+//     })
+
+//     explorer.addEventListener('click', function () {
+//         if (explorerStyle.getPropertyValue('display') === 'none') {
+//             partnerForm.style.display = 'none';
+//             partner.style.background = '#FFFFFF';
+//             partner.style.color = "#1E1E1E";
+            
+//             explorerForm.style.display = 'flex';
+//             explorer.style.color = '#FFFFFF';
+//             explorer.style.background = '#1E1E1E';
+//         } 
+//     })
+// };
+
+const switchForm = function (explorer, partner, explorerStyle, partnerStyle, explorerForm, partnerForm) {
+    for (let i = 0; i < 6; i++) {
+        partner.addEventListener('click', function () {
+            if (partnerStyle.getPropertyValue('display') === 'none') {
+                partnerForm.style.display = 'flex';
+                partner.style.backgroundColor = '#1E1E1E';
+                partner.style.color = "#FFFFFF";
+                
+                explorerForm.style.display = 'none';
+                explorer.style.color = '#1E1E1E';
+                explorer.style.backgroundColor = '#FFFFFF';
+            } 
+        })
+    
+        explorer.addEventListener('click', function () {
+            if (explorerStyle.getPropertyValue('display') === 'none') {
+                partnerForm.style.display = 'none';
+                partner.style.backgroundColor = '#FFFFFF';
+                partner.style.color = "#1E1E1E";
+                
+                explorerForm.style.display = 'flex';
+                explorer.style.color = '#FFFFFF';
+                explorer.style.backgroundColor = '#1E1E1E';
+            } 
+        })
+    }
+};
+
+function willRename() {
+    // switchForm(
+    //     loginHealthExplorer, 
+    //     loginHealthPartner, 
+    //     healthExplorerLoginStyle,
+    //     healthPartnerLoginStyle, 
+    //     healthExplorerLoginForm, 
+    //     healthPartnerLoginForm
+    // );
+    
+    switchForm(
+        signUphealthExplorer, 
+        signUpHealthPartner, 
+        healthExplorerSignUpStyle,
+        healthPartnerSignUpStyle, 
+        healthExplorerSignUpForm, 
+        healthPartnerSignUpForm
+    );
+    
+}
+
+willRename()
+//
